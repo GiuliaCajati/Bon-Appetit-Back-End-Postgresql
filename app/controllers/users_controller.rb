ce7b10dc1d65a8334a55360067860b535a5de056
giuliacajati <giuliacajati@gmail.com>
2020-11-09 09:50:35 -0500

    user login path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,30 @@
 class UsersController < ApplicationController
 
     def create
+        debugger
         #create user account 
         @user = User.new(name: params[:user][:name], password_digest: params[:user][:password], photo_url: params[:user][:photo_url])
         if @user.save 
             #upon success... render json response 
-            render json: @user.to_json 
+            render json: @user.to_json(include: [:meals])
         else 
             #upon failure... render json response 
             render json: {message: "This user is not authenticated"}
         end
     end 
 
-    def show 
+    def login 
         @user = User.find_by(name: params[:user][:name])
-        user_url = rails_blob_path(user.photo_url)
-        debugger
-        if @user && @user.authenticate(params[:user][:password])
+        
+        if @user.password == params[:password]
+            # @user && @user.authenticate(params[:user][:password])
             #upon success... render json response  
             render json: @user.to_json(include: [:meals])
         else
             #upon failure... render json response 
             render json: { message: "This user is not authenticated" }
         end  
+        debugger
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 
   post "/users", to: "users#create", as: "sign_up"
-  post "/login", to: "users#show"
+  post "/login", to: "users#login", as: "login"
   
   # get "/logout", to: "users#logout", as: "logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
