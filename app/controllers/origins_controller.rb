db9096572815bbea637d4434b1ff48957b06c135
giuliacajati <giuliacajati@gmail.com>
2020-11-10 12:11:32 -0500

    fixed origin controller/ route

--- app/controllers/origins_controller.rb
+++ app/controllers/origins_controller.rb
@@ -1,4 +1,4 @@
-class OriginssController < ApplicationController
+class OriginsController < ApplicationController
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  resources :ingredients
+  resources :origins
   resources :meals
-  resources :meal_ingredients
+  
 
 
 
