6c01c956c8cfa85c89e4db2e469c11d4909013df
giuliacajati <giuliacajati@gmail.com>
2020-11-11 12:53:42 -0500

    add meal

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -11,6 +11,20 @@ class MealsController < ApplicationController
         render json: @meal.to_json
     end 
 
+    def create 
+        @meal = Meal.create(name: params[:meal][:name], 
+            photo_url: params[:meal], 
+            likes:0, 
+            instructions: params[:meal][:instructions], 
+            ingredients: params[:meal][:ingredients],
+            user_id:params[:meal][:user_id], 
+            origin_id:params[:meal][:origin_id])
+        render json: @meals.to_json(include: :origin) 
+    end 
+
+
+    
+
     def show 
         @meal = Meal.find(params[:id])
         render json: @meal.to_json(include: :origin) 
